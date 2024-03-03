3cfa80d7c2182ae5df96813d47b6126193f5afde
AnatolyGavrilov <sharamradov@gmail.com>
2024-03-03 18:34:57 +0300

    Added yandex map

--- src/modules/Users/components/UsersMap/UsersMap.tsx
+++ src/modules/Users/components/UsersMap/UsersMap.tsx
@@ -1,9 +1,19 @@
 import { FC } from "react";
-
+import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps";
 const UsersMap: FC<any> = ({ users }) => {
+  const defaultState = {
+    center: [55.751574, 37.573856],
+    zoom: 5,
+  };
+
   return (
     <div>
       <h1>Карта пользователей</h1>
+      <YMaps>
+        <Map defaultState={defaultState}>
+          <Placemark geometry={[55.684758, 37.738521]} />
+        </Map>
+      </YMaps>
     </div>
   );
 };
