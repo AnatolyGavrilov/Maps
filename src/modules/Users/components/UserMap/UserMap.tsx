a0dfc6e505eb9abcea24d5b73c22343a626e9e57
AnatolyGavrilov <sharamradov@gmail.com>
2024-03-14 15:23:37 +0300

    refactored

--- src/modules/Users/components/UserMap/UserMap.tsx
+++ src/modules/Users/components/UserMap/UserMap.tsx
@@ -3,17 +3,17 @@ import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps";
 import { IUserMapProps } from "./UserMap.types";
 import styles from "./styles.module.scss";
 
-export const UserMap: FC<IUserMapProps> = ({ users }) => {
-  const defaultState = {
-    center: [55.751574, 37.573856],
-    zoom: 5,
-  };
+const MapState = {
+  center: [55.751574, 37.573856],
+  zoom: 5,
+};
 
+export const UserMap: FC<IUserMapProps> = ({ users }) => {
   return (
     <div className={styles.mapContainer}>
       <h1>Карта пользователей</h1>
       <YMaps>
-        <Map defaultState={defaultState} width={1640} height={700}>
+        <Map defaultState={MapState} width={1640} height={700}>
           {users.map((user) => (
             <Placemark
               key={user.id}
